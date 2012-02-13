1b8a68b015fb9d4f7e83e2a1a0dae113932fedcd
ahaarnos <ahaarnos@gmail.com>
2012-02-12 21:21:54 -0500

    Repo Creation is Go!  This is not really a working version, but it's a start.

---
+++ config.py
@@ -0,0 +1,55 @@
+"""
+Set various configuration variables for each run here.  All values need to be 
+rigourously documented!
+"""
+
+# This is the seed for the random number generator.  Each run's sequence of
+# random numbers is completely determined by this variable.  This enables us
+# to run multiple runs with the same sequence of random numbers.
+seed = 66143 # any integer
+#numpy.random.randint(100000000) is a good way to get a new one (higher than 4 billion is unadvisable).
+
+# System-as-a-whole parameters
+## Delta_t of each step through the simulation
+delta_t       = 1000 # (micro-seconds)
+## Duration of the simulation
+duration      = 600000 * delta_t # (either multiply delta_t or set a raw time - in microseconds)
+#how often should we print out the current system status?
+printOutAt    = 10000 #(a raw number of timesteps into the simulation)
+#how often should we wite the system status to a file?
+writeSysOutAt = 100 #(a raw number of timesteps into the simulation)
+#Prefix of the names of the files to output to (the timestamp of the run will also be included as a suffix)
+fileOut       = 'Spring2012_Data/tf10b100-20min_clus0.8-r1'
+## Dimensions of the system as a whole.  These values are in nm!
+systemWidth   = 1000 # x-axis (nm)
+systemLength  = 1000 # y-axis (nm)
+systemHeight  = 1000 # z-axis (nm)
+
+
+# Various parameters relating to the Transcription Factor (TF) molecules.
+## This value is the size of the TF molecules; if they are cubes, this is the
+## side length, and if they are spheres, this is their diameter.
+TFsize       = 10/3.0 # (nm)
+## The TF Diffusion Coefficient describes the scale of the random, Brownian
+## movement exhibited by TF molecules.
+TFdiffusionC = 10000 # (nm^2/s)
+TFnumberInit = 10 # This is the number of TF molecules at system start
+
+
+# Binding Site Parameters
+## This value is the size of binding sites; if they are cubes, this is the
+## side length, and if they are spheres, this is their diameter.
+bindSize     = 10 # (nm)
+bindSizeSquared = bindSize**2 #for expediency of the simulation
+## Probability of binding for a TF which within the volume of the binding site
+## during a single time-step of the system.
+pBind        = 1  # absolute probability [0,1]
+## Probability of unbinding for a TF which within the volume of the binding
+## site during a single time-step of the system.
+pUnbind      = (1.0 / (25.0 * (1.0 / (delta_t * 10**-6))))  # absolute probability [0,1] expressed this way, 25 seconds is the average bound time
+bindSitesNum = 100 # Number of binding sites in the system
+clustering   = True # whether or not binding sites should apppear in clusters
+## probability of each placed binding site to be within the cluster (if enabled)
+clusterProbability = 0.8
+## side-length/diameter of the cluster region
+clusterSize = 3 * bindSize * bindSitesNum * clusterProbability # (nm)
---
+++ initialization.py
@@ -0,0 +1,345 @@
+
+#Perform some initial setup
+import numpy
+import scipy
+import pylab
+import time
+
+import config as _conf
+from config import *
+
+def checkCollision(newNode, existingNodes, nodeSize):
+    sizeSquared = nodeSize**2 #avoid performing the same calculaion repeatedly!
+    for node in existingNodes:
+        if (((newNode[0]-node[0])**2 + (newNode[1]-node[1])**2 + (newNode[2]-node[2])**2) < sizeSquared):
+            print "collision averted"
+            return True
+    return False
+
+def placeBindSites(bindSites, clusters=False, cSize=0, cProbability=0):
+    i=0
+    if clusters==False:
+        while (i<len(bindSites)):
+            bindSites[i][0] = numpy.random.uniform(-maxX + TFsize/2.0,maxX-TFsize/2.0)
+            bindSites[i][1] = numpy.random.uniform(-maxY+TFsize/2.0,maxY-TFsize/2.0)
+            bindSites[i][2] = numpy.random.uniform(-maxZ+TFsize/2.0,maxZ-TFsize/2.0)
+            bindSites[i][3] = -1 #all initially unbound
+            bindSites[i][4] = False #none are in clusters here
+            if not checkCollision(bindSites[i],bindSites[0:i],bindSize):
+                i+=1
+    else:
+        print "Running With with clustering and a clusterprobability of " + str(clusterProbability)
+        clusterX = numpy.random.uniform(-maxX + clusterSize/2.0,maxX-clusterSize/2.0)
+        clusterY = numpy.random.uniform(-maxY + clusterSize/2.0,maxY-clusterSize/2.0)
+        clusterZ = numpy.random.uniform(-maxZ + clusterSize/2.0,maxZ-clusterSize/2.0)
+        while (i<len(bindSites)):
+            rand = numpy.random.uniform(0,1)
+            if (rand <= cProbability): #we place this one in a cluster!
+                placed = 0
+                while (placed==0): #place them in the cube of the cluster (maybe I should make this a sphere in the future)
+                    bindSites[i][0] = clusterX + numpy.random.uniform(-clusterSize/2.0,+clusterSize/2.0)
+                    bindSites[i][1] = clusterY + numpy.random.uniform(-clusterSize/2.0,+clusterSize/2.0)
+                    bindSites[i][2] = clusterZ + numpy.random.uniform(-clusterSize/2.0,+clusterSize/2.0)
+                    bindSites[i][3] = -1 #all initially unbound
+                    bindSites[i][4] = True #in a cluster
+                    if not checkCollision(bindSites[i],bindSites[0:i],bindSize):
+                        placed=1
+            else:
+                bindSites[i][0] = numpy.random.uniform(-maxX + TFsize/2.0,maxX-TFsize/2.0)
+                bindSites[i][1] = numpy.random.uniform(-maxY+TFsize/2.0,maxY-TFsize/2.0)
+                bindSites[i][2] = numpy.random.uniform(-maxZ+TFsize/2.0,maxZ-TFsize/2.0)
+                bindSites[i][3] = -1 #all initially unbound
+                bindSites[i][4] = False #in a cluster
+            i+=1
+        return (clusterX,clusterY,clusterZ)
+    return
+
+def placeTFs(tfs, size):
+    i=0
+    while (i<len(tfs)):
+        tfs[i][0] = numpy.random.uniform(-maxX + TFsize/2.0,maxX-TFsize/2.0)
+        tfs[i][1] = numpy.random.uniform(-maxY+TFsize/2.0,maxY-TFsize/2.0)
+        tfs[i][2] = numpy.random.uniform(-maxZ+TFsize/2.0,maxZ-TFsize/2.0)
+        tfs[i][3] = -1 #set them all unbound initially
+        i+=1
+    return
+
+def checkDiffusionCollision(node, allNodes, allNodesPrev):
+    #do nothing here now, yet... maybe never at all, since collisions are kind of irrevelant
+    return
+
+def diffuseTFs(TFs,TFsPrev,bindSites,dt,diffC):
+    #treating each dimension as a gaussian probability of movement, with sigma = (2Dt)^.5
+    sigma = (2*dt*(10**-6)*diffC)**.5 #The 10^-6 is due to dt being in _microseconds_
+    i=0
+    while (i<len(TFs)):
+        if (TFs[i][3] == -1): #only currently unbound elements may move!
+            randArr = numpy.random.normal(0,sigma,3)
+            TFs[i][0] += randArr[0]
+            if (TFs[i][0] > (maxX - TFsize/2.0)): #prevent leaving the box!
+                TFs[i][0] = 2.0*(maxX - TFsize/2.0) - TFs[i][0]
+            if (TFs[i][0] < (-maxX + TFsize/2.0)): #prevent leaving the box!
+                TFs[i][0] = 2.0*(-maxX + TFsize/2.0) - TFs[i][0]
+            TFs[i][1] += randArr[1]
+            if (TFs[i][1] > (maxY - TFsize/2.0)): #prevent leaving the box!
+                TFs[i][1] = 2.0*(maxY - TFsize/2.0) - TFs[i][1]
+            if (TFs[i][1] < (-maxY + TFsize/2.0)): #prevent leaving the box!
+                TFs[i][1] = 2.0*(-maxY + TFsize/2.0) - TFs[i][1]
+            TFs[i][2] += randArr[2]
+            if (TFs[i][2] > (maxZ - TFsize/2.0)): #prevent leaving the box!
+                TFs[i][2] = 2.0*(maxZ - TFsize/2.0) - TFs[i][2]
+            if (TFs[i][2] < (-maxZ + TFsize/2.0)): #prevent leaving the box!
+                TFs[i][2] = 2.0*(-maxZ + TFsize/2.0) - TFs[i][2]
+        i+=1
+    return
+    
+def iterate():
+    TFarrayPrev = TFarray.copy()
+    TFunbind()
+    diffuseTFs(TFarray,TFarrayPrev,bindSitesPosn,delta_t,TFdiffusionC)
+    TFbind()
+    return
+    
+def TFunbind(): #allow TFs to become unbound based on their probability of doing so
+    i=0
+    while (i<len(TFarray)):
+        if (TFarray[i][3] !=-1 ):
+            c = numpy.random.uniform(0.0,1.0)
+            if (c <= pUnbind):
+                bindSitesPosn[TFarray[i][3]][3] = -1 #unbind site
+                siteNum = TFarray[i][3]
+                TFarray[i][3] = -1 #unbind TF
+                print "UNBIND!"
+                if (i == tfToTrack):
+                    writeTFStatus(t + delta_t , i)
+                if (clustering and clusterProbability<1):
+                    if (siteNum == nonClusterSiteToTrack):
+                        writeSiteStatus(t + delta_t , siteNum, False) # False for the non-cluster site
+                    elif (siteNum == clusterSiteToTrack):
+                        writeSiteStatus(t + delta_t , siteNum)
+                else:
+                    if (siteNum == siteToTrack):
+                        writeSiteStatus(t + delta_t , siteNum)
+        i+=1
+    return
+
+def TFbind():
+    i=0
+    unboundSites = filter(lambda x: x[3] != -1, bindSitesPosn)
+    while (i<len(TFarray)):
+        if (TFarray[i][3] == -1 ):
+            siteNum = checkTFsiteCollision(TFarray[i],unboundSites)
+            if (siteNum != -1):
+                c = numpy.random.uniform(0.0,1.0)
+                if (c <= pBind):
+                    bindSitesPosn[siteNum][3] = i #bind site
+                    TFarray[i][3] = siteNum #bind TF
+                    print "BIND!"
+                    if (i == tfToTrack):
+                        writeTFStatus(t + delta_t , i)
+                    if (clustering and uSitesNum):
+                        if (siteNum == nonClusterSiteToTrack):
+                            writeSiteStatus(t + delta_t , siteNum, False) # False for the non-cluster site
+                        elif (siteNum == clusterSiteToTrack):
+                            writeSiteStatus(t + delta_t , siteNum)
+                    else:
+                        if (siteNum == siteToTrack):
+                            writeSiteStatus(t + delta_t , siteNum)
+                        
+        i+=1
+    return
+
+def checkTFsiteCollision(tf,bindSites):
+    i=0
+    while (i<len(bindSites)):
+        if (bindSitesPosn[siteNum][3] != -1): # ensure only unbound sites are considered for binding
+            distSquared = (bindSites[i][0] - tf[0])**2 + (bindSites[i][1] - tf[1])**2 + (bindSites[i][2] - tf[2])**2
+            if (distSquared <= bindSizeSquared):
+                return i
+                i+=1
+    return -1 #no collision
+
+def calcGlobalStats():
+    stats = {} #an associative array
+    #Percentage of TFs Bound
+    tfNum = len(TFarray) #in case we allow them to ever be created or destroyed
+    tfBound = 0
+    tfx = 0
+    tfy = 0
+    tfz = 0
+    for tf in TFarray:
+        tfx += tf[0] #for center of mass of the TFs calculation
+        tfy += tf[1]
+        tfz += tf[2]
+        if (tf[3] != -1):
+            tfBound +=1
+    stats['tfPercentBound'] = float(tfBound)/tfNum
+    #Percentage of Binding Sites Occupied
+    bNum = len(bindSitesPosn) #in case we allow them to ever be created or destroyed (less likely than with TFs)
+    bBound = 0
+    for b in bindSitesPosn:
+        if (b[3] != -1):
+            bBound +=1
+    stats['bindPercentOccupied'] = float(bBound) / bNum
+    #TF Center of Mass
+    cm = numpy.zeros((3))
+    cm[0] = tfx / tfNum
+    cm[1] = tfy / tfNum
+    cm[2] = tfz / tfNum
+    stats['tfmu-x'] = cm[0]
+    stats['tfmu-y'] = cm[1]
+    stats['tfmu-z'] = cm[2]
+    return stats
+#to output results, x = calcGlobalStats(); for i in x {print str(i)+':' + str(x[i])}
+
+def writeSysStatus(time,verbose=False): #output current systen status to a file -- need to think about output format and what data is useful
+    #verbose is intended to mean write out the status of the entire system
+    # (as in all positions of binding sites and tfs), but I'm not sure how to
+    # make that gnuplot-friendly
+    sysStats = calcGlobalStats()
+    if (time==0):
+        statsComment = '#timestamp(mu-s)'
+    statsValues = str(time)
+    statNames = sysStats.keys()
+    statNames.sort()
+    for statName in statNames:
+        if (time==0):
+            statsComment += ','+statName
+        statsValues += '\t'+str(sysStats[statName])
+    if (time==0):
+        systemFileOut.write(statsComment+'\n')
+    systemFileOut.write(statsValues+'\n')
+    return
+
+def writeTFStatus(time,tf):
+    if (time==0):
+        tfComment = '#TF being tracked: #'+str(tf)+' (of '+str(len(TFarray))+')\n#timestamp(mu-s),x,y,z,status,boundTo(-1 means unbound!)'
+    tfVals = str(time)+'\t'+str(TFarray[tf][0])+'\t'+str(TFarray[tf][1])+'\t'+str(TFarray[tf][2])+'\t'+str(int(TFarray[tf][3]!=-1))+'\t'+str(TFarray[tf][3])
+    if (time==0):
+        tfFileOut.write(tfComment+'\n')
+    tfFileOut.write(tfVals+'\n')
+    return
+
+def writeSiteStatus(time,site, c = True): # c is False for the non-cluster site, when applicable
+    if (c):
+        fOut = bindSiteFileOut
+    else:
+        fOut = bindSiteFileOut2
+    if (time==0):
+        siteComment = '#Site being tracked: #'+str(site)+' (of '+str(len(bindSitesPosn))+')\n'
+        siteComment += '#located at position: '+str(bindSitesPosn[site][0])+'\t'+str(bindSitesPosn[site][1])+'\t'+str(bindSitesPosn[site][2])+'\n'
+        if bindSitesPosn[site][4]:
+            siteComment += 'site is within a cluster'
+        siteComment += '#timestamp(mu-s),status,boundTo(-1 means unbound!)'
+        fOut.write(siteComment+'\n')
+
+    siteVals = str(time)+'\t'+str(int(bindSitesPosn[site][3]!=-1))+'\t'+str(bindSitesPosn[site][3])
+    fOut.write(siteVals+'\n')
+    return
+
+def printStatus(time, verbose=False):
+    print "System currently at time " + str(time) + "mu-s of total " + str(duration)
+    if (verbose):
+        print "\n TF Info:"
+        for tf in TFarray:
+            print tf[0:4]
+
+def writeConfig(f):
+    f.write('### Configuration used to generate this run ###\n')
+    var = dir(_conf)
+    var.sort(key=str.upper)
+    for v in var:
+        if (v[0] != '_'):
+            f.write('# ' + v + ': ' + str(eval(v)) + '\n') #output the name of v, then the value of v
+    f.write('\n\n')
+    return
+
+def runSimulation():
+    global t
+    iteration = 0
+    
+    while (t < duration):
+        if (iteration % printOutAt == 0):
+            printStatus(t, False)
+        if (iteration % writeSysOutAt == 0):
+            writeSysStatus(t)
+        iterate();
+        t += delta_t
+        iteration += 1
+    print "Simulation Complete"
+    return
+
+#initial variable settings
+numpy.random.seed(seed)
+
+maxX = systemWidth/2
+maxY = systemLength/2
+maxZ = systemHeight/2
+
+#the array containing all the positions of the binding sites
+bindSitesPosn = numpy.zeros((bindSitesNum,5)) # values 0, 1, and 2 are posn values. value 3 is the boundStatus (-1 is not bound!)
+TFarray = numpy.zeros((TFnumberInit,4)) # values 0, 1, and 2 are posn values. value 3 is the boundStatus (-1 is not bound!)
+
+#create initial files and write the config to the top of each, to ensure the config and data are never separated
+start_t = time.localtime() #all files will share the same timestamp as part of their name
+timestamp = str(start_t.tm_mon)+'-'+str(start_t.tm_mday)+'-'+str(start_t.tm_year)+'-h'+str(start_t.tm_hour)+'m'+str(start_t.tm_min)+'s'+str(start_t.tm_sec)
+systemFileOut = open(fileOut+'-system-'+timestamp,'w',0)
+tfFileOut = open(fileOut+'-TF-'+timestamp,'w',0)
+bindSiteFileOut = open(fileOut+'-bindingSite-'+timestamp,'w',0)
+if (clustering and clusterProbability<1):
+    bindSiteFileOut2 = open(fileOut+'-bindingSite(non-cluster)-'+timestamp,'w',0)
+    writeConfig(bindSiteFileOut2)
+writeConfig(systemFileOut)
+writeConfig(tfFileOut)
+writeConfig(bindSiteFileOut)
+
+t=0
+try:
+    #The actual flow of the program occurs below.
+    clusterPosn = placeBindSites(bindSitesPosn,clustering,clusterSize,clusterProbability)
+    if (clusterPosn):
+        systemFileOut.write("# Cluster centered at: " + str(clusterPosn) + "\n\n")
+    placeTFs(TFarray,TFsize)
+    TFarrayPrev = TFarray.copy()
+
+    unclusteredSites = filter(lambda x: x[4] == False, bindSitesPosn)
+    uSitesNum = len(unclusteredSites)    
+    
+    print "TF's and Binding Sites now in place."
+
+    if (clustering):
+        print "Cluster centered at: " + str(clusterPosn) + "\n"
+    
+    print "Binding Sites are fixed at the following locations:"
+    systemFileOut.write("# Binding Sites are fixed at the following locations:\n")
+    for site in bindSitesPosn:
+        print site
+        systemFileOut.write("# " + str(site) + "\n")
+    systemFileOut.write("\n\n")
+    print "\n TFs are initially located here:"
+    for tf in TFarray:
+        print tf[0:3]
+
+    print "Beginning time evolution..."
+
+    tfToTrack = numpy.random.randint(len(TFarray))
+    writeTFStatus(0,tfToTrack)
+    if (clustering and clusterProbability<1):
+        clusterSiteToTrack = numpy.random.randint(len(bindSitesPosn))
+        nonClusterSiteToTrack = numpy.random.randint(len(bindSitesPosn))
+        while (not bindSitesPosn[clusterSiteToTrack][4]):
+            clusterSiteToTrack = numpy.random.randint(len(bindSitesPosn))
+        writeSiteStatus(0, clusterSiteToTrack)
+        while (bindSitesPosn[nonClusterSiteToTrack][4]):
+            nonClusterSiteToTrack = numpy.random.randint(len(bindSitesPosn))
+        writeSiteStatus(0, nonClusterSiteToTrack, False)
+    else:
+        siteToTrack = numpy.random.randint(len(bindSitesPosn))
+        writeSiteStatus(0, siteToTrack)
+
+    
+    runSimulation()
+finally: #this way the files are closed even if an error is encountered
+    systemFileOut.close()
+    tfFileOut.close()
+    bindSiteFileOut.close()
